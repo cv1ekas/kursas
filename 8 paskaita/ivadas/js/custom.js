d68684d5b224fb13b4c282c48405874a533d9725
Zilvinas <cv1ekass@gmail.com>
2019-08-03 10:52:22 +0300

    ikelta

---
+++ 8 paskaita/ivadas/js/custom.js
@@ -0,0 +1,11 @@
+//apsiraseme funkcija(kavos aparato pastatymas i kampa)
+function sudekDuSkaicius(x, y) {
+	let suma = x + y;
+	console.log(suma);
+	document.write("<h1>"+suma+"</h1>");
+}
+
+function atimti(x, y){
+	let rezultatas = x - y;
+	return rezultatas;
+}
---
+++ 8 paskaita/salygos/js/custom.js
@@ -0,0 +1,39 @@
+//function arSkaiciusDidesnisUz10(skaicius){
+//	
+//	if(skaicius > 10){
+//		document.write("Skaicius" + skaicius + "didesnis uz 10");
+//	}
+//	
+//	else{
+//		document.write("Skaicius" + skaicius + "mazesnis uz 10");
+//	}
+function arSkaiciusDidesnisUz10(skaicius){
+	
+	let statusas = "";
+	
+	if(skaicius > 10){
+		statusas = "didesnis";
+	}
+	else if(skaicius == 10 ){
+		statusas = "lygus";
+	}
+	
+	else{
+		statusas = "mazesnis uz";
+	}
+		document.write("Skaicius" + skaicius + " " + statusas + " 10<br>");
+		
+}
+
+
+function arTeisingasSlaptazodis(paduodamasSlaptazodis){
+	let esamasSlaptazodis = "qwerty123";
+
+	if (paduodamasSlaptazodis == esamasSlaptazodis ){
+		document.write("teisingas<br>");
+	}
+	else {
+		document.write("neteisingas<br>");
+
+	}
+}
