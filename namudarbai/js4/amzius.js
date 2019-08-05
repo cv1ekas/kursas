a43319766aa98e42c3bf4ed3329518bd842debd4
Zilvinas <cv1ekass@gmail.com>
2019-08-05 18:25:19 +0300

    .

--- namudarbai/js4/amzius.js
+++ namudarbai/js4/amzius.js
@@ -2,9 +2,9 @@ function arPilnametis(amzius){
 
 	if(amzius >= 18){
 		document.write("jūs galite užeiti į svetainę<br>");
-}
+	}
 	else{
 		document.write("į svetainę užeiti negalite<br>");
-}
+	}
 }
 
