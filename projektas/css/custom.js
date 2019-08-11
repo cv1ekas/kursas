2854d538b842f20e132753cca8dbd520e2aad20c
Zilvinas <cv1ekass@gmail.com>
2019-08-11 18:51:19 +0300

    pataisyta

---
+++ projektas/css/custom.js
@@ -0,0 +1,43 @@
+function padetiAsmeniIMasyva(vardas, pavarde, amzius, asmenys){
+	let asmuo = {
+		vardas: vardas,
+		pavarde: pavarde,
+		email: email
+	};
+
+	asmenys.push(asmuo);
+
+	return asmenys;
+}
+
+function ziuretiDaugiau() {
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("myBtn");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Rodyti daugiau";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Rodyti mažiau";
+    moreText.style.display = "inline";
+  }
+}
+function ziuretiDaugiau2() {
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("more2");
+  var btnText = document.getElementById("myBtn2");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Rodyti daugiau";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Rodyti mažiau";
+    moreText.style.display = "inline";
+  }
+}
+
