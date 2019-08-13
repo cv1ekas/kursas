927ef4e5838c976f11eebf65c980bcfcfaba387a
Zilvinas <cv1ekass@gmail.com>
2019-08-13 16:53:51 +0300

    .

---
+++ namudarbai/php01/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="">
+<head>
+			<meta charset="utf-8">
+			<meta name="viewport" content="width=device-width, initial-scale=1.0">
+			<title></title>
+</head>
+
+<body>
+	<?php
+		$country='Lietuva';
+		$city='Vilnius';
+		$population=574221;
+
+		echo '<ul>';
+		echo '<li>';
+		echo $country;
+		echo '</li>';
+
+		echo '<li>';
+		echo $city;
+		echo '</li>';
+
+		echo '<li>';
+		echo $population;
+		echo '</li>';
+
+		echo	'</ul>';
+	?>
+</body>
+</html>
