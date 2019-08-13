9b56ebf7b4a1950797bec761fe4c71eefda17041
Zilvinas <cv1ekass@gmail.com>
2019-08-13 16:53:08 +0300

    .

---
+++ 11 paskaita/svetaines turinio destymas1/js/service.js
@@ -0,0 +1,19 @@
+function getGallery(){
+	return [
+		{ imageLink: "https://picsum.photos/id/599/800/800", title: "Kalnai", id: 1},
+		{ imageLink: "https://picsum.photos/id/520/800/800", title: "Tiltai", id: 2},
+		{ imageLink: "https://picsum.photos/id/225/800/800", title: "Arbata", id: 3},
+		{ imageLink: "https://picsum.photos/id/950/800/800", title: "Horizontas", id: 4},
+		{ imageLink: "https://picsum.photos/id/1056/800/800", title: "Debesys", id: 5},
+		{ imageLink: "https://picsum.photos/id/1057/800/800", title: "Kalnai ir debesys", id: 6},
+		{ imageLink: "https://picsum.photos/id/1058/800/800", title: "Aikštelė", id: 7},
+		{ imageLink: "https://picsum.photos/id/1059/800/800", title: "Paroda", id: 8},
+		{ imageLink: "https://picsum.photos/id/1060/800/800", title: "Kava", id: 9},
+		{ imageLink: "https://picsum.photos/id/1061/800/800", title: "Jūra", id: 10},
+		{ imageLink: "https://picsum.photos/id/1062/800/800", title: "Šuo", id: 11},
+		{ imageLink: "https://picsum.photos/id/1063/800/800", title: "Miškas ir kelias", id: 12},
+		{ imageLink: "https://picsum.photos/id/1064/800/800", title: "Miškas ir rūkas", id: 13},
+		{ imageLink: "https://picsum.photos/id/1065/800/800", title: "Gatvė į jūrą", id: 14},
+		{ imageLink: "https://picsum.photos/id/1066/800/800", title: "Kūdikis", id: 15}
+	];
+}
