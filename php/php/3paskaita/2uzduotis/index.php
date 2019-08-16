71c44618f705fa087ac91361182760b10c405a71
Zilvinas <cv1ekass@gmail.com>
2019-08-16 14:49:13 +0300

    .

--- php/php/3paskaita/2uzduotis/index.php
+++ php/php/3paskaita/2uzduotis/index.php
@@ -9,24 +9,20 @@
 
 <body>
 	<form action="<?php $_PHP_SELF; ?>" method="POST">
-		Kraštinės aukštis: <input type="text" name="aukstis"/><br><br>
-		Kraštinės ilgis: <input type="text" name="ilgis"/><br>
-		<input type="submit" name="submit" value="skaičiuoti">
+	Kraštinės aukštis: <input type="text" name="aukstis"/><br>
+	Kraštinės ilgis: <input type="text" name="ilgis"/>
+	<input type="submit" name="submit" value="skaičiuoti">
 	</form>
 
 	<?php
 		if(isset($_POST['submit'])){
-			$aukstis = $_POST['aukstis'];
-			$ilgis = $_POST['ilgis'];
-			$plotas = $aukstis*$ilgis;
+		$aukstis = $_POST['aukstis'];
+		$ilgis = $_POST['ilgis'];
+		$plotas = $aukstis*$ilgis;
 		echo "Stačiakampio plotas kurio kraštinės
-			$aukstis ir $ilgis yra $plotas";
+		$aukstis ir $ilgis yra $plotas";
 }
 	?>
-
-
-
-
 </body>
 </html>
 
